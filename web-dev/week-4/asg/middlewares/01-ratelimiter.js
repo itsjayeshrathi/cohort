f49786610663e2a08837f58a06e0c79eb23953eb
Jayesh Rathi <itsjayeshrathi@gmail.com>
2024-12-12 20:13:07 +0000

    created rate limiter

--- web-dev/week-4/asg/middlewares/01-ratelimiter.js
+++ web-dev/week-4/asg/middlewares/01-ratelimiter.js
@@ -1,23 +1,35 @@
 // You have to create a middleware for rate limiting a users request based on their username passed in the header
-import express from "express";
+import express, { json } from "express";
 const app = express();
 app.use(json());
-const userNames = {};
-const reqCount = 0;
-const checker = setInterval(() => {
-  if (reqCount > 5) {
-    return res.send(`Wait before you send another request`);
+const users = {};
+let request_count = 1;
+// setInterval(() => {}, 1000);
+app.use((req, res, next) => {
+  let user_name = req.header.user;
+  if (users["user_name"] && request_count >= 5) {
+    users["user_name"] = 0;
+    request_count = 1;
+    return res.send("Wait before you send anymore request");
   } else {
-    return next();
+    next();
   }
-}, 1000);
-app.use(checker());
-app.get("/", (req, res) => {
-  const name = req.headers.name;
-  if (userNames.name) {
-  
-  } else {
-    userNames = { ...userNames, name: reqCount };
-  } 
 });
+app.get("/", (req, res) => {
+  let user_name = req.header.user;
+  if (users["user_name"]) {
+    console.log("here");
+    request_count = request_count + 1;
+    users["user_name"] = request_count;
+  } else {
+    console.log("here too");
+    users["user_name"] = request_count;
+  }
+  res.send(`your request count is ${users["user_name"]}`);
+});
+const PORT = 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
 export default app;
